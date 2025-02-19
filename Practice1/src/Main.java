28304778420f6d49c3927316a2a8f444af4beb72
Max Zenko <zenkomm@gmail.com>
2025-02-19 16:42:15 +0200

    Папки добавлены

---
+++ Practice1/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("ВВедите любое 5 значное число:");
+            String num = in.nextLine();
+            String res = num.replaceAll("[^0-9]", "");
+            if (res.length() != 5) {
+                System.out.println("Ошибка: число должно содержать ровно 5 цифр.");
+                return;
+            }
+            System.out.println("Цифры числа:");
+            for (int i = 0; i < res.length(); i++) {
+                System.out.println(res.charAt(i));
+            }
+        }
+    }
+}
---
+++ Practice2/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите сторону a: ");
+        double a = in.nextDouble();
+        System.out.print("Введите сторону b: ");
+        double b = in.nextDouble();
+        System.out.print("Введите сторону c: ");
+        double c = in.nextDouble();
+        //полупериметр
+        double p = (a + b + c) / 2;
+        //формула Герона
+        double res = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        System.out.println("Площадь треугольника: " + res);
+    }
+    }
+}
